77b6a6498f1ea661d475003abd0c2ae98807cc3c
collapse <58087001+espalloc7@users.noreply.github.com>
2023-12-06 16:17:13 +0300

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,20 @@
+import requests
+
+# Read the endpoints from the text file
+with open('endpoints.txt', 'r') as f:
+    endpoints = f.read().splitlines()
+
+alive = []
+for endpoint in endpoints:
+    try:
+        response = requests.get(endpoint)
+        # If the response does not contain '401', add it to the list
+        if '401' not in response.text:
+            alive.append(endpoint)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred with endpoint {endpoint}: {e}")
+
+# Write the alive urls to a new text file
+with open('alive.txt', 'w', encoding='utf8') as f:
+    for url in alive:
+        f.write(url + '\n')
